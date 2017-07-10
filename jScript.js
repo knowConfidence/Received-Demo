a07cc67f86beb1488fe41c68e89e0d763219e72f
knowConfidence <gsjheeta55@hotmail.com>
2017-07-10 15:25:41 +0100

    Add files via upload

---
+++ jScript.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+	setCardCenter();
+
+	$(window).resize(function(){
+
+		setCardCenter();
+
+	});
+
+
+});
+
+
+function setCardCenter(){
+
+	var card = $(".card_wrapper");
+
+	card.find("img").removeAttr("height");
+	card.find("img").attr("width", "100%");
+	card.find(".card_image_wrapper").css("left", 0);
+
+	var imgHeight = card.find(".card_image").outerHeight();
+	var textHeight = card.find(".card_text").outerHeight();
+
+	var height = 0;
+
+	var which = false;
+
+	if(imgHeight > textHeight){
+
+		height = imgHeight;
+		which = false;
+	}
+	else{
+
+		height = textHeight;
+		which = true;
+	}
+
+	card.css("height", height);
+
+	toggleWidthHeight(card, height, which);
+
+}
+
+function toggleWidthHeight(card, height, which){
+
+	if(which){
+
+		card.find("img").attr("height", height+"px");
+		card.find("img").removeAttr("width");
+
+		var imgHeight = card.find("img").width();
+		var imgWrapper = card.find(".card_image_wrapper").outerWidth();
+
+		var f = imgWrapper - imgHeight;
+
+		card.find(".card_image_wrapper").css("left", f);
+
+	}
+}
